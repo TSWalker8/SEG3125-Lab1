6ec2f3b031398dc1550a85af11c175ff9682dc5d
Matthew Walker <Walker@Matthews-MacBook-Pro.local>
2020-01-21 11:11:23 -0500

    Welcome Screen

--- app/src/main/java/com/coding/walker/seg3125_lab1/WelcomeScreen.java
+++ app/src/main/java/com/coding/walker/seg3125_lab1/WelcomeScreen.java
@@ -2,12 +2,54 @@ package com.coding.walker.seg3125_lab1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class WelcomeScreen extends AppCompatActivity {
+    private Spinner options;
+    private Button select;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
+        addItemsOnSpinner();
+        options.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        select= findViewById(R.id.Select);
+        select.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    public void addItemsOnSpinner() {
+
+        options = findViewById(R.id.Options);
+        List<String> list = new ArrayList<String>();
+        list.add("Boating License");
+        list.add("A+ Certification");
+        list.add("G1 License");
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, list);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        options.setAdapter(dataAdapter);
     }
 }
